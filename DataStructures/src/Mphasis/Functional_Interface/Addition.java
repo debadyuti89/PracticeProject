e813ddc6b9c1f70b3256bae998ac678cbb4e8b7d
Debadyuti <Debadyuti@Debadyuti-G15-Laptop>
2025-07-09 19:12:03 +0530

    adding my changes

---
+++ DataStructures/src/Mphasis/Functional_Interface/Addition.java
@@ -0,0 +1,7 @@
+package Mphasis.Functional_Interface;
+
+@FunctionalInterface
+public interface Addition {
+	int add(int a, int b);
+	//Object addq(int a, int b);
+}
---
+++ DataStructures/src/practice/Employee.java
@@ -0,0 +1,50 @@
+package practice;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Employee {
+
+	private String name;
+	private int id;
+	private double salary;
+
+	public Employee() {
+		super();
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+
+	public double getSalary() {
+		return salary;
+	}
+
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [name=" + name + ", id=" + id + "]";
+	}
+
+
+}
